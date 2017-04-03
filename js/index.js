08668d7574785064ce3c28b6d0e00a7634a79ac3
AlexWebArchitect <alex.web.architect@gmail.com>
2017-04-03 20:02:29 +0300

    Stream status is visible and accurate now.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
 function  apicall() {
     $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/freecodecamp?callback=?', function (fccdata) {
+        var fccstatus = fccdata.stream;
+        if (fccstatus == null) $("#fccstatus").html("offline");
+        else $("#fccstatus").html("online");
         console.log(fccdata);
     });
     $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/ESL_SC2?callback=?', function (esldata) {
+        var eslstatus = esldata.stream;
+        if (eslstatus == null) $("#eslstatus").html("offline");
+        else $("#eslstatus").html("online");
         console.log(esldata);
     });
 }
